9acc33ed75d4506ada059435263cca9d27fd05c0
stamovm <m.stamov@yahoo.com>
2021-08-18 23:45:27 -0700

    project 2 is completed

--- p-js/fcc-algo.js
+++ p-js/fcc-algo.js
@@ -1,86 +1,88 @@
-var globalArray = [5, 6, 3, 2, 9];
+var globalArray = [5, 6, 3, 2, 9]
 function nonMutatingSort(arr) {
-  let arr2 = arr.concat([]); //copy arr to arr2
-  return arr2.sort((a, b) => a - b);
+  let arr2 = arr.concat([]) //copy arr to arr2
+  return arr2.sort((a, b) => a - b)
 }
 // console.log(nonMutatingSort(globalArray));
 
-var otherString = "How9are7you2today";
-var byDigits = otherString.split(/\d/);
+var otherString = 'How9are7you2today'
+var byDigits = otherString.split(/\d/)
 
 function splitify(str) {
-  return str.split(/[\d\s-,]/); //spit to arr of words from str separated by digits spaces or - and ,
+  return str.split(/[\d\s-,]/) //spit to arr of words from str separated by digits spaces or - and ,
 }
 // console.log(splitify("Hello World,I-am code"));
 
 function sentensify(str) {
-  let arr = str.split(/[-.,]+/);
-  str = arr.join(" ");
-  console.log(str);
+  let arr = str.split(/[-.,]+/)
+  str = arr.join(' ')
+  console.log(str)
 }
 // sentensify("May-the.force,--...,be-with-you");
 
 function urlSlug(title) {
-  let arr = title.toLowerCase().trim().split(/\s+/);
-  title = arr.join("-");
+  let arr = title.toLowerCase().trim().split(/\s+/)
+  title = arr.join('-')
 
-  console.log(title);
+  console.log(title)
 }
 // urlSlug(" Winter Is  Coming");
 
 function pairElement(str) {
   const pair = (ch) => {
-    let arr = [];
-    arr.push(ch);
+    let arr = []
+    arr.push(ch)
     switch (ch) {
-      case "A":
-        arr.push("T");
-        break;
-      case "T" || "C":
-        arr.push("A");
-        break;
-      case "C":
-        arr.push("G");
-        break;
-      case "G":
-        arr.push("C");
-        break;
+      case 'A':
+        arr.push('T')
+        break
+      case 'T' || 'C':
+        arr.push('A')
+        break
+      case 'C':
+        arr.push('G')
+        break
+      case 'G':
+        arr.push('C')
+        break
     }
-    return arr;
-  };
-
-  let arr = [];
-  for (let i = 0; i < str.length; i++) {
-    arr.push(pair(str[i]));
+    return arr
   }
-  return arr;
+
+  let arr = []
+  for (let i = 0; i < str.length; i++) {
+    arr.push(pair(str[i]))
+  }
+  return arr
 }
 
 // pairElement("ATCGA"); //should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]
 
 function fearNotLetter(str) {
-  let arr = [];
+  let arr = []
   for (let i = 0; i < str.length; i++) {
-    arr.push(str.charCodeAt(i));
+    arr.push(str.charCodeAt(i))
   }
   for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i] != arr[i + 1] - 1) {
-      return String.fromCharCode(arr[i] + 1);
+      return String.fromCharCode(arr[i] + 1)
     }
   }
-  return undefined;
+  return undefined
 }
 // console.log(fearNotLetter("abce"));
 
 function uniteUnique(arr) {
-  let arr2 = [];
+  let arr2 = []
   for (let i = 0; i < arguments.length; i++) {
     arguments[i].forEach((num) => {
-      if (arr2.indexOf(num) < 0) arr2.push(parseInt(num));
-    });
+      if (arr2.indexOf(num) < 0) arr2.push(parseInt(num))
+    })
   }
-  console.log(arr2);
-  return arr2;
+  console.log(arr2)
+  return arr2
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])
+let i = 0
+i++
---
+++ p2/script.js
@@ -0,0 +1,39 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let cActive = 1
+next.addEventListener('click', () => {
+  cActive++
+  if (cActive > circles.length) cActive = circles.length
+  update()
+})
+
+prev.addEventListener('click', () => {
+  cActive--
+  if (cActive < 1) cActive = 1
+  update()
+})
+
+function update() {
+  circles.forEach((c, idx) => {
+    if (idx < cActive) {
+      c.classList.add('active')
+    } else {
+      c.classList.remove('active')
+    }
+  })
+  const actives = document.querySelectorAll('.active')
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
+  console.log(cActive)
+  if (cActive === 1) {
+    prev.disabled = true
+  } else if (cActive == circles.length) {
+    next.disabled = true
+  } else {
+    prev.disabled = false
+    next.disabled = false
+  }
+}
